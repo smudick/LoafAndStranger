da2864df5defcf87869efbf76c4ecb03554b8f8f
Sam Mudick <smudick@gmail.com>
2021-02-20 14:05:04 -0600

    saturday class

---
+++ LoafAndStranger/Controllers/LoavesController.cs
@@ -0,0 +1,66 @@
+﻿using LoafAndStranger.DataAccess;
+using LoafAndStranger.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LoafAndStranger.Controllers
+{
+    [Route("api/Loaves")]
+    [ApiController]
+    public class LoavesController : ControllerBase
+    {
+        LoafRepository _repo;
+        public LoavesController()
+        {
+            _repo = new LoafRepository();
+        }
+        //GET to /api/loaves
+        [HttpGet]
+        public IActionResult GetAllLoaves()
+        {
+            return Ok(_repo.GetAll());
+        }
+        //POST to /api/loaves
+        [HttpPost]
+        public IActionResult AddALoaf(Loaf loaf)
+        {
+            _repo.Add(loaf);
+            return Created($"api/Loaves/{loaf.Id}", loaf);
+        }
+
+        //GET to /api/loaves/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var loaf = _repo.Get(id);
+
+            if (loaf == null)
+            {
+                return NotFound("This loaf id does not exist");
+            }
+            return Ok(loaf);
+        }
+
+        //Idempotency -> Idempotent
+        //PUT to /api/loaves/{id}/slice
+        [HttpPut("{id}/slice")]
+        public IActionResult SliceLoaf(int id)
+        {
+            var loaf = _repo.Get(id);
+            loaf.Sliced = true;
+            return NoContent();
+        }
+        //Delete /api/loaves/{loafid}
+        [HttpDelete("{loadId}")]
+        public IActionResult PurchaseLoaf(int loafId)
+        {
+            _repo.Remove(loafId);
+            return Ok();
+        }
+    }
+}
---
+++ LoafAndStranger/DataAccess/LoafRepository.cs
@@ -0,0 +1,39 @@
+﻿using LoafAndStranger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoafAndStranger.DataAccess
+{
+    public class LoafRepository
+    {
+        static List<Loaf> _loaves = new List<Loaf>
+            {
+                 new Loaf { Id = 1, Price = 5.50, Size = LoafSize.Medium, Sliced = true, Type= "Rye"},
+                 new Loaf { Id = 2, Price = 2.50, Size = LoafSize.Small, Sliced = false, Type= "French"},
+            };
+        public List<Loaf> GetAll()
+        {
+            return _loaves;
+        }
+        public void Add(Loaf loaf)
+        {
+            var biggestExistingId = _loaves.Max(l => l.Id);
+            loaf.Id = biggestExistingId + 1;
+            _loaves.Add(loaf);
+        }
+        public Loaf Get(int id)
+        {
+            var loaf = _loaves.FirstOrDefault(bread => bread.Id == id);
+            return loaf;
+
+        }
+
+        public void Remove(int id)
+        {
+            var loafToRemove = Get(id);
+            _loaves.Remove(loafToRemove);
+        }
+    }
+}
